5af7a676846094861e6aafe66e2cc24894699b47
Juan Toledo <>
2023-01-13 00:22:28 +0100

    Async function test OK

--- src/store/action.test.js
+++ src/store/action.test.js
@@ -1,13 +1,49 @@
-import { advertsLoadedSucess } from "./actions";
+import {
+  advertsLoadedSucess,
+  authLoginRequest,
+  authLoginNotSet,
+  authLoginSucess,
+  authLoginFailure,
+} from "./actions";
 import { ADVERTS_LOADED_SUCESS } from "./types";
 
-test("devolver una accion de tipo success", () => {
-  const adverts = "adverts";
-  const expectedAction = {
-    type: ADVERTS_LOADED_SUCESS,
-    payload: adverts,
-  };
+describe("advertsLoadedSucess", () => {
+  test("devolver una accion de tipo success", () => {
+    const adverts = "adverts";
+    const expectedAction = {
+      type: ADVERTS_LOADED_SUCESS,
+      payload: adverts,
+    };
 
-  const action = advertsLoadedSucess(adverts);
-  expect(action).toEqual(expectedAction);
+    const action = advertsLoadedSucess(adverts);
+    expect(action).toEqual(expectedAction);
+  });
+});
+
+describe("authLoginNotSet", () => {
+  const credentials = "credenciales";
+  const action = authLoginNotSet(credentials);
+  const dispatch = jest.fn();
+  const api = { auth: {} };
+
+  describe("cuando resuelve", () => {
+    test("hacer el flujo de login", async () => {
+      api.auth.loginNotSet = jest.fn().mockResolvedValue();
+      await action(dispatch, undefined, { api });
+      expect(dispatch).toHaveBeenNthCalledWith(1, authLoginRequest());
+      expect(api.auth.loginNotSet).toHaveBeenCalledWith(credentials);
+      expect(dispatch).toHaveBeenNthCalledWith(2, authLoginSucess());
+    });
+  });
+
+  describe("cuando rechaza", () => {
+    const error = new Error("error");
+    test("hacer el flujo de errores", async () => {
+      api.auth.loginNotSet = jest.fn().mockRejectedValue(error);
+      const promise = action(dispatch, undefined, { api });
+      await expect(promise).rejects.toThrowError(error);
+      expect(dispatch).toHaveBeenNthCalledWith(1, authLoginRequest());
+      expect(dispatch).toHaveBeenNthCalledWith(2, authLoginFailure(error));
+    });
+  });
 });
